aa0cc679facffd3b7ee8213ab01c1dffd22d6467
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-02-24 20:53:19 +0900

    Parameter setting finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 
 class ApplicationController < ActionController::API
-
         include DeviseTokenAuth::Concerns::SetUserByToken
         before_action :configre_permitted_parameters, if: :devise_controller?
         protected
          def configre_permitted_parameters
-           devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+           devise_parameter_sanitizer.permit(:sign_up, keys: [:name] )
+           devise_parameter_sanitizer.permit(:account_update, keys: [:name,:email,:avatar])
          end
+
 end
 
 # def skip_session
