46a0db1780bfdb077d374f4c20ec1e334e5345b0
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-06-16 22:15:57 +0900

    Password change api test completed

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -63,7 +63,11 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
   describe "アカウント設定" do
     let(:user) { FactoryBot.create(:other) }
     before do
-      @params=auth_post user, api_v1_user_session_path, params:{email:user.email,password:"000000"}
+      @params=auth_post user, api_v1_user_session_path,
+      params:{
+        email:user.email,
+        password:"000000"
+      }
     end
     describe "PUT /api/v1/auth #update" do
         context "アカウント設定ができる" do
@@ -133,6 +137,103 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
             end
           end
        end
+    end
+  end
+
+  describe "パスワード変更する" do
+    let(:user) { FactoryBot.create(:other) }
+    before do
+      @params=auth_post user, api_v1_user_session_path, params:{email:user.email,password:"000000"}
+    end
+    describe "POST /api/v1/auth/password #create" do
+      describe "パスワードリセット確認メール" do
+        context "確認メールを送信ができる" do
+          it "Emailアドレスが存在する。" do
+            post api_v1_user_password_path,params:{email:user.email,redirect_url:"http://localhost:3001/confirmation/password"}
+            res = JSON.parse(response.body)
+            expect(response.status).to eq 200
+            expect(res['message']).to include ('にパスワードリセットの案内が送信されました。')
+          end
+        end
+        context "確認メールを送信ができない" do
+          it "Emailアドレスが存在しない" do
+            post api_v1_user_password_path,params:{email:"ka@example.com",redirect_url:"http://localhost:3001/confirmation/password"}
+            res = JSON.parse(response.body)
+            expect(response.status).to eq 404
+            expect(res['errors']).to include ("メールアドレス 'ka@example.com' のユーザーが見つかりません。")
+          end
+          it "Emailアドレスが指定されていない" do
+            post api_v1_user_password_path,params:{email:nil,redirect_url:"http://localhost:3001/confirmation/password"}
+            res = JSON.parse(response.body)
+            expect(response.status).to eq 401
+            expect(res['errors']).to include ("メールアドレスが与えられていません。")
+          end
+          it "redirect_urlが指定されていない" do
+            post api_v1_user_password_path,params:{email:"ka@example.com",redirect_url:nil}
+            res = JSON.parse(response.body)
+            expect(response.status).to eq 401
+            expect(res['errors']).to include ("リダイレクト URL が与えられていません。")
+          end
+        end
+      end
+    end
+    describe "PUT /api/v1/auth/password #update" do
+      describe "パスワード変更" do
+        context "パスワードが変更できる" do
+            it "tokenが存在する" do
+              put api_v1_user_password_path,params:{password:"999999",password_confirmation:"999999"},headers:@params[:headers]
+              res = JSON.parse(response.body)
+              expect(response.status).to eq(200)
+              expect(res['message']).to include("パスワードの更新に成功しました。")
+            end
+            it "変更したパスワードでログインができる" do
+              put api_v1_user_password_path,params:{password:"999999",password_confirmation:"999999"},headers:@params[:headers]
+              res = JSON.parse(response.body)
+              res=auth_post user, api_v1_user_session_path, params:{email:@params[:params][:email],password:"999999"}
+              expect(res[:status]).to eq(200)
+           end
+           it "パスワード変更後、変更前のパスワードではログインができない" do
+             put api_v1_user_password_path,params:{password:"999999",password_confirmation:"999999"},headers:@params[:headers]
+             res = JSON.parse(response.body)
+             res=auth_post user, api_v1_user_session_path, params:{email:@params[:params][:email],password:@params[:params][:password]}
+             res2 = JSON.parse(response.body)
+             expect(res[:status]).to eq(401)
+             expect(res2['errors']).to include("ログイン用の認証情報が正しくありません。再度お試しください。")
+           end
+        end
+        context "パスワードが変更できない" do
+          it "headersが空" do
+            put api_v1_user_password_path,params:{password:"999999",password_confirmation:"999999"},headers:{}
+            res = JSON.parse(response.body)
+            expect(response.status).to eq(401)
+            expect(res['errors']).to include("Unauthorized")
+          end
+          it "passwordとpassword_confirmationの値が違う" do
+            put api_v1_user_password_path,params:{password:"999999",password_confirmation:"999000000"},headers:@params[:headers]
+            res = JSON.parse(response.body)
+            expect(response.status).to eq(422)
+            expect(res['errors']['full_messages']).to include("パスワード（確認用）とパスワードの入力が一致しません")
+          end
+          it "パスワードが6文字以内" do
+            put api_v1_user_password_path,params:{password:"99",password_confirmation:"99"},headers:@params[:headers]
+            res = JSON.parse(response.body)
+            expect(response.status).to eq(422)
+            expect(res['errors']['full_messages']).to include("パスワードは6文字以上で入力してください")
+          end
+          context "ログインしてから14日後" do
+            around do |e|
+              travel_to('2021-2-29 10:00'.to_time){ e.run }
+            end
+            it "tokenの有効期限が過ぎた" do
+              travel 15.day
+              put api_v1_user_password_path,params:{password:"999999",password_confirmation:"999000000"},headers:@params[:headers]
+              res = JSON.parse(response.body)
+              expect(response.status).to eq 401
+              expect(res['errors']).to include("Unauthorized")
+            end
+          end
+        end
+      end
     end
   end
 end
