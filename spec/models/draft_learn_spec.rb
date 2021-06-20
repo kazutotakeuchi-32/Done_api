a74470a6ab0825f439e6563269314453391007f9
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-06-20 20:40:44 +0900

    Confirmation of circleci behavior

--- spec/models/draft_learn_spec.rb
+++ spec/models/draft_learn_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe DraftLearn, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "失敗するテスト" do
+    expect(1+1).to eq 10
+  end
 end
