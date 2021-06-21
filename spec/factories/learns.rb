658efe0ad52f3e4e8ab7527885b54f206ba025ff
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-06-21 22:33:48 +0900

    Learning completion planning function Unit test completed

--- spec/factories/learns.rb
+++ spec/factories/learns.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :learn do
-    
+    title{"Rubyの基礎文法を学ぶ"}
+    content {"プロゲートでRubyをクリアする。"}
+    subject {"プログラミング"}
+    time {10}
+    user_id {nil}
+    draft_learn_id {nil}
   end
 end
