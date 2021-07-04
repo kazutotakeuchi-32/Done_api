075a24f571ad0ab3a06fd74ec389c70fb2263e70
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-07-04 22:06:11 +0900

    Image upload API test completed

--- app/controllers/direct_uploads_controller.rb
+++ app/controllers/direct_uploads_controller.rb
@@ -1,21 +1,21 @@
 class DirectUploadsController < ActiveStorage::DirectUploadsController
   skip_before_action :verify_authenticity_token
   def create
-    blob = ActiveStorage::Blob.create_before_direct_upload!(blob_args)
-    render json: direct_uploads_json(blob)
+      blob = ActiveStorage::Blob.create_before_direct_upload!(blob_args)
+      render json: direct_uploads_json(blob),status:200
   end
   def update
     user=User.find(params[:user_id])
     blob=ActiveStorage::Blob.find(params[:blob][:id])
     user.image.attach(blob)
-    render json: {imageUrl:url_for(user.image_blob)}
+    render json: {imageUrl:url_for(user.image_blob)},status:200
   end
   def destroy
     user=User.find(params[:id])
-    user.image.pupurge
+    user.image.purge
     user.avatar = ""
     user.save
-    render json: {stauts: 200}
+    render json: {message:"ok"},stauts: 200
   end
   private
     def direct_uploads_json(blob)
