2b595cf5307a544ec303dafcb16abe4af616ea45
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-07-12 22:26:56 +0900

    Notification function unit test completed

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cuurent_user){FactoryBot.create(:test_user1)}
+  let(:other_user){FactoryBot.create(:test_user2)}
+  let(:room){Room.create}
+  describe "通知" do
+    before do
+      @message=Message.create(user_id:cuurent_user.id,room_id:room.id,message:"test")
+      @notification=Notification.create(sender_id:cuurent_user.id,receiver_id:other_user.id,message_id:@message.id,kind:"DM")
+    end
+    it "１つの通知は１人の送信者に紐づく" do
+      expect(@notification.sender).to eq (cuurent_user)
+    end
+    it "１つの通知は１人の受信者に紐づく" do
+      expect(@notification.receiver).to eq (other_user)
+    end
+    it "１つの通知は１メッセージに紐づく" do
+      expect(@notification.message).to eq (@message)
+    end
+  end
 end
